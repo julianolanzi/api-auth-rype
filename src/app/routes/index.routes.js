2e03e5f6844842924e7abf9fc5e2e7c84948f10c
julianolanzi <julianolanzi@hotmail.com>
2023-06-05 19:29:56 -0300

    ajuste final para CI e CD

--- src/app/routes/index.routes.js
+++ src/app/routes/index.routes.js
@@ -4,7 +4,7 @@ const version = require('../../../package.json');
 
 router.get('/', (req, res, next) => {
     res.status(200).send({
-        title: 'Rype API Auth Onlines',
+        title: 'Rype API Auth Online',
         version: version.version
     });
 })
