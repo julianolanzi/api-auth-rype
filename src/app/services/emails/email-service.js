956f37bc307734b2b1c989054472054ec7e01ccf
julianolanzi <julianolanzi@hotmail.com>
2023-06-06 08:30:13 -0300

    removendo arquivos nao utilizados

--- src/app/services/emails/email-service.js
+++ src/app/services/emails/email-service.js
@@ -2,21 +2,6 @@ const configs = require('../../config/configs');
 const sendgrid = require('@sendgrid/mail');
 sendgrid.setApiKey(configs.SENDGRID.SENDGRID_API_KEY);
 
-exports.registerMessage = (user) => {
-    return {
-        to: user.email,
-        from: {
-            name: 'RypeGG',
-            email: 'contato@rypegg.com'
-        },
-        templateId: configs.registerEmailId,
-        dynamicTemplateData: {
-            nickname: user.nickname,
-            name: user.name,
-            email: user.email
-        }
-    };
-}
 
 exports.forgotPassword = (user, url) => {
 
